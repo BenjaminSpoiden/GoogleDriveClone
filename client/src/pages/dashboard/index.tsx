2d32d36a1a495cc4de0e4a0409b8b78a016fa776
BenjaminSpoiden <spoiden.benjamin@hotmail.com>
2021-02-06 10:54:50 +0100

    protected route for dashboard

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -1,6 +1,19 @@
 import { Heading } from "@chakra-ui/react"
+import { useRouter } from "next/dist/client/router"
+import { useEffect } from "react"
+import { useAuth } from "../../hooks/useAuth"
 
 const Dashboard = () => {
+
+    const router = useRouter()
+    const { user, loading } = useAuth()
+
+    useEffect(() => {
+        if(!user && !loading) {
+            router.replace("/")
+        }
+    }, [user, loading])
+
     return (
         <Heading>
             USERS ONLY
